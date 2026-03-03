acccab16ef0918cc98e6e9f0145f9952a9eb7cf9
Alibek Khudoykulov <66941344+AlibekKhudoykulov@users.noreply.github.com>
2026-03-03 09:48:15 +0500

    Two Pointers solution for findMiddle

--- src/linked-lists/FindAMiddle.java
+++ src/linked-lists/FindAMiddle.java
@@ -1,18 +1,36 @@
 class FindAMiddle{
-     public static LinkedListNode<Integer> findMiddle(LinkedListNode<Integer> head) {
-        LinkedListNode<Integer> current = head;
-        int count = 0;
-        while (current != null) {
-            count++;
-            current = current.next;
-        }
-        
-        current = head;
-        for(int i = 0; i < (count / 2) ; i++){
-            current = current.next;
+  public static LinkedListNode<Integer> findMiddle(LinkedListNode<Integer> head) {
+        LinkedListNode<Integer> mid = head;
+        LinkedListNode<Integer> fast = head;
+
+        while (fast != null && fast.next!=null) {
+            mid = mid.next;
+            fast = fast.next.next;
         }
 
         // Replace this placeholder return statement with your code
-        return current;
+        return mid;
+    }
+    
+     public static void main(String[] args) {
+
+        Integer[][] input = {
+            {7, 10, 14, 21, 22},
+            {3, 6, 9, 12},
+            {23, 19, 15, 22, 34, 76, 12},
+            {5},
+            {1, 3, 5, 7, 9, 11,6},
+        };
+
+        for (int i = 0; i < input.length; i++) {
+			LinkedList<Integer> inputLinkedList = new LinkedList<Integer>();
+            inputLinkedList.createLinkedList(input[i]);
+            System.out.print((i + 1) + ".\tInput linked list: ");
+            PrintList<Integer> printLinkedList = new PrintList<Integer>();
+            printLinkedList.printListWithForwardArrow(inputLinkedList.head);
+            System.out.print("\n\tMiddle Node: " + findMiddle(inputLinkedList.head).data);
+            System.out.println();
+            System.out.println(new String(new char[100]).replace('\0', '-'));
+        }
     }
 }
