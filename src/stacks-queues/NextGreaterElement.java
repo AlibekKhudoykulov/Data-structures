8bcba7027053b552c816c72941dc4a87c32aa45a
Alibek Khudoykulov <66941344+AlibekKhudoykulov@users.noreply.github.com>
2026-03-04 15:37:20 +0500

    Solution:  NextGreaterElement with stacks

--- src/stacks-queues/NextGreaterElement.java
+++ src/stacks-queues/NextGreaterElement.java
@@ -1,16 +1,40 @@
-class NextGreaterElement{
-      public static int[] nextGreaterElement(int[] arr) {
-        int[] result = new int[arr.length];
-        for(int i = 0; i < arr.length; i++){
-            for(int j = i + 1; j < arr.length; j++){
-                if(arr[i] < arr[j]){
-                    result[i] = arr[j];
-                    break;
-                }
-                if(j==arr.length-1) result[i] = -1;
+import java.util.*;
+
+class NextGreaterElement {
+    public static int[] nextGreaterElement(int[] arr) {
+        MyStack < Integer > stack = new MyStack < > ();
+        int[] res = new int[arr.length];
+        Arrays.fill(res, -1);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            // While stack has elements and the current element is greater than peek element, pop all elements
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
             }
-            if(i==arr.length-1) result[i] = -1;
+
+            // If the stack has an element, the peek element will be greater than ith element
+            if (!stack.isEmpty()) {
+                res[i] = stack.peek();
+            }
+            stack.push(arr[i]);
         }
-        return result;
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] inputs = {
+            {4, 6, 3, 2, 8, 1, 9, 9, 9},
+            {33, 20, 105, 88},
+            {12, 5, 44, 56, 46, 78},
+            {1, 2, 3, 4, 5},
+            {150, 125, 100, 75, 50, 25, 0}
+        };
+
+        for (int i = 0; i < inputs.length; i++) {
+            System.out.println((i + 1) + "\tArray: " + Arrays.toString(inputs[i]));
+            System.out.println("\n\tResult: " + Arrays.toString(nextGreaterElement(inputs[i])));
+            System.out.println(new String(new char[100]).replace('\0', '-'));
+        }
     }
 }
